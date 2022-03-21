e2fd0df796dc4bff1b14f648bb7d2699b8f7efed
Usuario <Usuario@DESKTOP-ITEJBTN>
2022-03-20 20:21:17 -0600

    semana8

--- src/main/java/com/tienda/dao/ClienteDao.java
+++ src/main/java/com/tienda/dao/ClienteDao.java
@@ -1,5 +1,5 @@
 
-package com.tienda.ClienteDao;
+package com.tienda.dao;
 
 import com.tienda.domain.Cliente;
 import org.springframework.data.repository.CrudRepository;
--- src/main/java/com/tienda/service/ClienteServiceimpl.java
+++ src/main/java/com/tienda/service/ClienteServiceimpl.java
@@ -1,8 +1,10 @@
 
 package com.tienda.service;
 
-import com.tienda.ClienteDao.ClienteDao;
+import com.tienda.dao.ClienteDao;
+import com.tienda.dao.CreditoDao;
 import com.tienda.domain.Cliente;
+import com.tienda.domain.Credito;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,9 @@ public class ClienteServiceimpl implements ClienteService {
     
     @Autowired
     private ClienteDao clienteDao;
+    
+    @Autowired
+    private CreditoDao creditoDao;
 
     @Override
     public List<Cliente> getClientes() {
@@ -21,6 +26,9 @@ public class ClienteServiceimpl implements ClienteService {
 
     @Override
     public void save(Cliente cliente) {
+        Credito credito = cliente.getCredito();
+        credito = creditoDao.save(credito);
+        cliente.setCredito(credito);
         clienteDao.save(cliente);
     }
 
